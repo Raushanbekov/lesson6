1709f618c860af0fe3cadcae228f8ae30ec98006
Ulukbek <ulukbek15.99@mail.ru>
2020-11-16 18:36:22 +0300

    add new route

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,9 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+
+def test_raushanbekov(app, client):
+    res = client.get('/raushanbekov')
+    assert res.status_code == 200
+    assert res.data == b"Hello from CI with GitHub Actions by Raushanbekov"
